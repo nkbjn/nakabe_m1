6c645894bcb0d2ab3ffcd6c108b12363b7e759ab
yodai-hi <hirano.yodai.hv4@is.naist.jp>
2019-08-01 02:29:40 +0900

    追加： Activity画面なし，Overlay layoutのみ表示 X 起動時にもう一度アプリを立ち上げると落ちる

--- app/src/main/java/jp/naist/ubi_lab/nakabe_m1/MainActivity.kt
+++ app/src/main/java/jp/naist/ubi_lab/nakabe_m1/MainActivity.kt
@@ -1,12 +1,32 @@
 package jp.naist.ubi_lab.nakabe_m1
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.provider.Settings
+import jp.naist.ubi_lab.nakabe_m1.service.OverlayService
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : Activity() {
+    private val CUSTOM_OVERLAY_PERMISSION_REQUEST_CODE = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        checkOverlay()
+        val serviceIntent = Intent(this, OverlayService::class.java)
+        startService(serviceIntent)
+        this.finishAndRemoveTask()
+    }
+
+    private fun checkOverlay() {
+        if (!Settings.canDrawOverlays(this)){
+            val intent =
+                Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + this.packageName))
+            startActivityForResult(
+                intent,
+                CUSTOM_OVERLAY_PERMISSION_REQUEST_CODE
+            )
+        }
     }
 }
