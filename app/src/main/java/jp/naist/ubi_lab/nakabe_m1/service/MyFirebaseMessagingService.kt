b4e586c16681403121eb1dee282bece79031b576
Jin_Nakabe <nakabe.jin.nb8@is.naist.jp>
2019-08-03 00:50:16 +0900

    FireBaseの通知が来るとこまでやったよ。 アプリがフォアグラウンドで実行しているときに、FCMからの内容をLogで出力できるようにしている状態。つまり、内部のメッセージに対する処理を行うことができる状態まで。 実際ばバックグラウンドで動かす必要があるので、バックグラウンドで通知が来たときの内容を観れるようにしたい。

--- app/src/main/java/jp/naist/ubi_lab/nakabe_m1/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/jp/naist/ubi_lab/nakabe_m1/service/MyFirebaseMessagingService.kt
@@ -3,6 +3,7 @@ package jp.naist.ubi_lab.nakabe_m1.service
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import java.lang.NullPointerException
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String?) {
@@ -13,14 +14,20 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
         remoteMessage?.let { message ->
             // 通知メッセージ
-            message.notification?.let {
+            message.notification?.let {notification ->
                 // 通知メッセージを処理
+                Log.i("FIREBASE2","[Service]  = Title = ${notification.title ?: "Empty2"}")
+                Log.i("FIREBASE3","[Service]  = body = ${notification.body ?: "Empty3"}")
             }
 
-            // データメッセージ
-            message.data?.let {
-                // データメッセージを処理
-            }
+            if (message.data == null) {
+                // データメッセージ
+                message.data?.let { data ->
+                    // データメッセージを処理
+                    Log.i("FIREBASE4", "[Service]  = Nick = $data")
+                    Log.i("FIREBASE5", "[Service]  = Room = $data")
+                }
+            }else Log.i("FIREBASE6", "空だぜ")
         }
     }
 }
